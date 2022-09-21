edd4147123d4c923615459db0050711e155cb8b2
mirianmag <magri.mirian@gmail.com>
2022-09-21 15:31:18 -0400

    feat: adding code to formate in USD

--- 7/util.js
+++ 7/util.js
@@ -2,5 +2,5 @@ function arredondar (valor) {
     return Math.round(valor*100)/100;
 }
 function formatMoney (value) {
-    return "$" + value;
+    return new Intl.NumberFormat("en-us", { currency: "USD", style: "currency"}).format(value);
 }
