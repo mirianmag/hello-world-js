90ad9af817b290e33b84afddc880010b30bd4327
mirianmag <magri.mirian@gmail.com>
2023-01-23 15:15:08 -0500

    refactoring: removing not used code

--- 8/main.js
+++ 8/main.js
@@ -4,6 +4,7 @@ janeiro.addTransaction(new Transaction("Salary", "income", 3000));
 janeiro.addTransaction(new Transaction("Rent", "expense", 1000)); 
 janeiro.addTransaction(new Transaction("Energy Bill", "expense", 200)); 
 janeiro.addTransaction(new Transaction("Water Bill", "expense", 100)); 
+janeiro.addTransaction(new Transaction("Internet", "expense", 100)); 
 
 const fevereiro = new Month("February");
 fevereiro.addTransaction(new Transaction("Salary", "income", 3000));
@@ -11,9 +12,6 @@ fevereiro.addTransaction(new Transaction("Rent", "expense", 1200));
 fevereiro.addTransaction(new Transaction("Energy Bill", "expense", 250)); 
 fevereiro.addTransaction(new Transaction("Water Bill", "expense", 100)); 
 fevereiro.addTransaction(new Transaction("Internet", "expense", 100));
-fevereiro.addTransaction(new Transaction("Transport", "expense", 500));
-fevereiro.addTransaction(new Transaction("Food", "expense", 1000));
-fevereiro.addTransaction(new Transaction("Condo", "expense", 400));   
 
 const marco = new Month("March");
 marco.addTransaction(new Transaction("Salary", "income", 4000)); 
@@ -21,10 +19,6 @@ marco.addTransaction(new Transaction("Rent", "expense", 1200));
 marco.addTransaction(new Transaction("Energy Bill", "expense", 200)); 
 marco.addTransaction(new Transaction("Water Bill", "expense", 100)); 
 marco.addTransaction(new Transaction("Internet", "expense", 200)); 
-marco.addTransaction(new Transaction("Transport", "expense", 500)); 
-marco.addTransaction(new Transaction("Hobby", "expense", 800)); 
-marco.addTransaction(new Transaction("Food", "expense", 1000));
-marco.addTransaction(new Transaction("Condo", "expense", 400));
 
 const year = new Year();
 year.addMonth(janeiro);
